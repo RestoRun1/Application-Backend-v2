fade87ee81c25cca3797d365d52d21568695f04f
Semih Kaan Cakir <kaan.cakir@ug.bilkent.edu.tr>
2024-03-28 18:29:18 +0300

    Problematic Junit.runner has been commented out, application properties has been changed for the app to connect to the RDS database

--- Backend-Application/src/test/java/com/restorun/backendapplication/model/RestaurantTest.java
+++ Backend-Application/src/test/java/com/restorun/backendapplication/model/RestaurantTest.java
@@ -2,7 +2,7 @@ package com.restorun.backendapplication.model;
 
 import com.restorun.backendapplication.model.Restaurant;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
