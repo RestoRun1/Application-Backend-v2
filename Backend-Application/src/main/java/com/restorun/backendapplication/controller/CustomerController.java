522cabb6c7cfb9d1798f5b4f8208e94bd936815f
Efe Can Tepe <59337173+efecantepe@users.noreply.github.com>
2024-04-22 00:19:27 +0300

    saveCustomer works. docker-compose.yml is added. init.sql is added. Properties changed. Port 6543

--- Backend-Application/src/main/java/com/restorun/backendapplication/controller/CustomerController.java
+++ Backend-Application/src/main/java/com/restorun/backendapplication/controller/CustomerController.java
@@ -1,12 +1,19 @@
 package com.restorun.backendapplication.controller;
 
+import com.restorun.backendapplication.enums.Role;
 import com.restorun.backendapplication.model.Customer;
 import com.restorun.backendapplication.service.CustomerService;
 
+import io.swagger.v3.core.util.Json;
+import jakarta.validation.constraints.Email;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -40,14 +47,39 @@ public class CustomerController {
     }
 
     @PostMapping("saveCustomer")
-    public ResponseEntity<String> saveCustomer(@RequestBody Customer customer) {
+    public ResponseEntity<String> saveCustomer(@RequestBody Map<String, Object> customerBody ) {
+
+        String username = (String) customerBody.get("username");
+        String email = (String) customerBody.get("email");
+        String password = (String) customerBody.get("password"); 
+        String confirmedPassword = (String) customerBody.get("confirmedPassword");
+
+        if(username.length() == 0){
+            return ResponseEntity.badRequest().body("Username cannot be empty");
+        }
+
+        if(!password.equals(confirmedPassword)){
+            return ResponseEntity.badRequest().body("password did not matched");
+        }
+
+        Customer customer = new Customer();
+        customer.setUsername(username);
+        customer.setEmail(email);
+        customer.setPassword(confirmedPassword);
+        customer.setRole(Role.CUSTOMER.getRole());
+        
         boolean saved = customerService.saveCustomer(customer);
         if (!saved) {
             return ResponseEntity.badRequest().build();
         }
+
+        System.out.println("Customer is" + customer.toString());
+
         return ResponseEntity.ok("Customer saved successfully");
     }
 
-    // planned business logic?
+    
+
+
 
 }
